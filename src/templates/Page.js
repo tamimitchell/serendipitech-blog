87b87fbbd34bbf8822d92b9984c811b6c1821316
Tami Mitchell <mail@tameramitchell.com>
2019-05-10 19:48:25 -0700

    Page Query

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,10 +1,33 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from 'gatsby'
 
-const PageTemplate = () => (
+const PageTemplate = ({ data }) => {
+  const page = data.airtable.data
+  
+  return (
   <Layout>
-    <h1>Page Template</h1>
+    <h1>{ page.Title }</h1>
+    <div dangerouslySetInnerHTML={{ __html: page.Body }} />
   </Layout>
-)
+)}
 
 export default PageTemplate
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    airtable(
+      table: {eq: "Pages"},
+      data: {
+        Published: {eq: true}, 
+        Slug: {eq: $slug}
+      })
+      {
+        data {
+          Title
+          Slug
+          Body
+        }
+      }
+  }
+`
