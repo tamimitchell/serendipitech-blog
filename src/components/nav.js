2014b1af67e57a1d796b18c6a78042f656ecc2a5
Tami Mitchell <mail@tameramitchell.com>
2019-05-11 18:09:58 -0700

    Clean up warnings

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import React from "react"
 
 const Nav = () => (
@@ -7,8 +7,6 @@ const Nav = () => (
       <li><a href="index.html">Home</a></li>
       <li><a href="generic.html">Generic</a></li>
       <li><a href="elements.html">Elements</a></li>
-      <li><a href="#" className="button primary">Sign Up</a></li>
-      <li><a href="#" className="button">Log In</a></li>
     </ul>
   </nav>
 )
