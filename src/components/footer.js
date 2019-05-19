89d838bf5b9eb9df2a9b6d5d01064a85695a7f26
Tami Mitchell <mail@tameramitchell.com>
2019-05-19 09:26:43 -0700

    Fix Twitter URL

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer id="footer">
     <div className="align-center">
       <ul className="icons">
-        <li><a href="https://twitter.com/serendipitech" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+        <li><a href="https://twitter.com/serendipi_tech" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
         <li><a href="https://www.facebook.com/SerendipiTech-346174432705596/" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="https://tamimitchell.com" className="icon fa-globe"><span className="label">Tami Mitchell</span></a></li>
         <li><a href="mailto:mail@tamimitchell.com" className="icon fa-envelope"><span className="label">Email</span></a></li>
