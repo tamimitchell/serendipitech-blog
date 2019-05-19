f1f832c8a71f1a0960f846572441aea31a46812f
Tami Mitchell <mail@tameramitchell.com>
2019-05-19 12:18:52 -0700

    Use default favicon settings

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import favicon from '../images/favicon.ico';
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
@@ -78,10 +77,6 @@ function SEO({ description, lang, meta, keywords, title }) {
             : []
         )
         .concat(meta)}
-      link={[
-          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
-          { rel: 'apple-touch-icon', type: 'image/png', href: `${favicon}` }
-      ]}
     />
   )
 }
