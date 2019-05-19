491db34786edd9cc6c8c66a4099b64743d54d65a
Tami Mitchell <mail@tameramitchell.com>
2019-05-19 13:08:15 -0700

    Gatsby favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
